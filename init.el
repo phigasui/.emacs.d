243d6ae584d9497defc1d260c488b9570c965e32
phigasui <phigasui@gmail.com>
2015-02-27 01:18:54 +0900

    20150227

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
-(setq load-path (append
-                 '("~/.emacs.d/elisp")
-                 load-path))
+(add-to-list 'load-path "~/.emacs.d/elisp")
+
+;; ELPA/Marmalade/MELPAパッケージの設定
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;; (setq url-http-attempt-keepalives nil) ;;To fix MELPA problem.
 
 
 ;;;; function ;;;;
@@ -8,10 +15,6 @@
   (interactive)
   (let (parens-require-spaces)
     (insert-pair)))
-
-;; (defun insert-env (env)
-;;   (interactive "sexec-env: ")
-;;   (insert (concat "#! " (shell-command-to-string (concat "which " env)) "\n")))
 
 (defun insert-env ()
   (interactive)
@@ -26,7 +29,7 @@
 ;;;; design ;;;;
 (set-face-background 'region "#DDD")
 
-(require 'cl)
+;; (require 'cl)
 (global-whitespace-mode t)
 (setq whitespace-display-mappings
       '((space-mark ?\ [?\ ])
@@ -99,6 +102,8 @@
       (cons (cons "\\.cmm$" 'c-mode) auto-mode-alist))
 (setq auto-mode-alist
       (cons (cons "\\.m$" 'octave-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons (cons "\\.wsgi$" 'python-mode) auto-mode-alist))
 
 
 ;;;; python ;;;;
@@ -111,8 +116,51 @@
  'python-mode-hook
  '(lambda()
     (define-key python-mode-map "\C-cm" 'py-main)
-    (define-key python-mode-map "\"" 'electric-pair)
-    (define-key python-mode-map "\'" 'electric-pair)
-    (define-key python-mode-map "[" 'electric-pair)
-    (define-key python-mode-map "{" 'electric-pair)
-    (define-key python-mode-map "(" 'electric-pair)))
+    ;; (define-key python-mode-map "\"" 'electric-pair)
+    ;; (define-key python-mode-map "\'" 'electric-pair)
+    ;; (define-key python-mode-map "[" 'electric-pair)
+    ;; (define-key python-mode-map "{" 'electric-pair)
+    ;; (define-key python-mode-map "(" 'electric-pair)
+    ))
+
+
+
+(add-to-list 'load-path "~/.emacs.d/emacs-swoop")
+(require 'swoop)
+(global-set-key (kbd "C-c o") 'swoop)
+
+;; rubikitch
+;; auto-install.elの設定
+(add-to-list 'load-path "~/.emacs.d/auto-install")
+(require 'auto-install)
+;; (auto-install-update-emacswiki-package-name t)
+;; (auto-install-compatibility-setup)
+;; (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+;; 試行錯誤用ファイルを開くための設定
+(require 'open-junk-file)
+;; C-x C-zで試行錯誤ファイルを開く
+(global-set-key (kbd "C-x C-z") 'open-junk-file)
+;; 式の評価結果を注釈するための設定
+;; (require 'lispxmp)
+;; emacs-lisp-modeでC-c C-dを押すと注釈される
+;; (define-key emacs-lisp-mode-map (kbd "C-c C-d") 'lispxmp)
+;; 括弧の対応を保持して編集する設定
+;; (require 'paredit)
+;; (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+;; (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+;; (add-hook 'lisp-mode-hook 'enable-paredit-mode)
+;; (add-hook 'ielm-mode-hook 'enable-paredit-mode)
+(require 'auto-async-byte-compile)
+;; 自動バイトコンパイルを無効にするファイル名の正規表現
+(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+;; (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+;; (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+;; (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+;; (setq eldoc-idle-delay 0.2) すぐに表示したい
+;; (setq eldoc-minor-mode-string "") モードラインにElDocと表示しない
+
+;; find-functionをキー割り当てする
+;; (find-function-setup-keys)
+
+;; (package-refresh-contents)
