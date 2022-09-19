fc2b7f7309766b8ee02a48991b7cbb44e4d6c773
phigasui <phigasui@gmail.com>
2022-09-19 23:12:24 +0900

    Introduce straight

--- init.el
+++ init.el
@@ -14,6 +14,18 @@
  ;; If there is more than one, they won't work right.
  )
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 ;; Package Managements
 (require 'package)
